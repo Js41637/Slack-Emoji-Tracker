aa801c434ca04412bf41470beb1786e2a029ff8e
Js41637 ftw <js41637@gmail.com>
2016-07-01 11:35:10 +1000

    user emoji

--- src/commands.js
+++ src/commands.js
@@ -3,10 +3,12 @@ import moment from 'moment'
 //import config from '../config.json'
 import { executeQuery } from './database'
 
+const noUsers = 281
+
 export default function parseCommand(user, text, getUser) {
   return new Promise(resolve => {
     let command = text.substring(1).split(' ')[0]
-      //let context = text.split(' ').slice(1).join(' ') || undefined
+    let context = text.split(' ').slice(1).join(' ') || undefined
     switch (command.toLowerCase()) {
       case 'topemoji':
         executeQuery('SELECT name as name, COUNT(*) as count FROM Emoji GROUP BY name ORDER BY COUNT(*) DESC LIMIT 1').then(results => {
@@ -36,6 +38,33 @@ export default function parseCommand(user, text, getUser) {
           })
         })
         break;
+      case 'useremojistats':
+      case 'useremoji':
+      case 'userstats':
+        {
+          if (!context) return resolve("Specify a user pls")
+
+          let dude = getUser(context.slice(0, 2) == "<@" ? context.slice(2, -1) : context)
+          let limit = 6
+          if (!dude) return resolve("No user by dat name m8")
+          executeQuery('SELECT name, date from Emoji WHERE user = \'' + dude.id + '\' ORDER BY date DESC LIMIT 1').then(newest => {
+            executeQuery('SELECT name, COUNT(*) as count from Emoji WHERE user = \'' + dude.id + '\' GROUP BY name ORDER BY COUNT(*) DESC LIMIT ' + limit).then(top => {
+              try {
+                let latest = newest.next().row
+                let topemoji = top.rs.rows._array
+                let out = [`*Emoji Statistics for ${dude.name}:* \n *Last Used*: :${latest.name}:  about ${moment().to(latest.date)} \n *Top ${topemoji.length} most used Emoji*:`]
+                topemoji.forEach(e => {
+                  out.push(`:${e.name}: - ${e.count}`)
+                })
+                out.push(`Not including ${noUsers} Emoji with no user data`)
+                return resolve(out.join('\n'))
+              } catch (e) {
+                return resolve("No stats for this user, I think, or something went horribly wrong")
+              }
+            })
+          })
+          break;
+        }
       default:
         return
     }
